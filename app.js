beac203c334b0d7159dc930da2d9edb9f624a177
Kathryn Lawton <klawton16@gmail.com>
2018-05-09 11:01:33 -0500

    adding updates to files

---
+++ app.js
@@ -0,0 +1,4 @@
+let hoursWorked = prompt("Please eneter your hours worked");
+let payRate = prompt("Please enter your pay rate");
+let payCalculator = hoursWorked * payRate;
+console.log(payCalculator);
